f3eebe3108599b47a2e3319f06dcdbe6333db054
SergeyShobuhin <shobuhin@mail.ru>
2024-03-14 15:50:33 +0700

    refactor

---
+++ controller/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace controller;
+
+use PDO;
+use PDOException;
+
+class Db
+{
+    private PDO $connection;
+
+    public function __construct()
+    {
+
+        $config = require "config/config.php";
+        $dsn = 'mysql:host=' . $config['mysql']['host'] . ';dbname=' . $config['mysql']['dbname'] . ';charset=' . $config['mysql']['charset'];
+
+        try {
+
+            $this->connection = new PDO($dsn, $config['mysql']['username'], $config['mysql']['password']);
+
+        } catch (PDOException $exception) {
+
+            print_r("Error: {$exception->getMessage()}");
+            throw new PDOException($exception->getMessage());
+        }
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+}
